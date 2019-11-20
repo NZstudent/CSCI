d83f48fae2c642064455624c26563e4dd614ead6
NZstudent <57472305+NZstudent@users.noreply.github.com>
2019-11-19 23:10:36 -0700

    Add files via upload

--- company/Main.java
+++ company/Main.java
@@ -25,85 +25,13 @@ public class Main {
             e.printStackTrace();
         }
 
-        String[] tempsArray = temps.toArray(new String[0]);
-
-
-        int y = -1;
-        for (String s : tempsArray) {
-            y++;
-            for(int i = 0; i < (s.length()-1); i++) {
-                System.out.print("Character: "+s.charAt(i));
-                Maze.generateMaze(s.charAt(i), y);
-            }
-            System.out.print("\n");
-
-            System.out.println("Maze" + Maze.maze[1][0]);
-        }
+        System.out.println(temps);
+        Maze.generateMaze((ArrayList<String>) temps);
 
         System.out.println("Maze::::::::::::::");
-        for(int i = 0; i <= 11; i++){
-            for(int j=0; j <= 11; j++){
-                System.out.println(Maze.maze[i][j]);
-            }
-
-        }
+        Maze.Print();
+        Maze.mazeRunner(Maze.maze, 0,2,0,3);
 
 
         }
     }
-
-
-
-//class FileOpen {
-//
-//    Scanner fin;
-//    int x;
-//    ArrayList[][] maze = new ArrayList[12][12];
-//    int y = 0;
-//    String ar[];
-//
-//    FileOpen(String fileName) {
-//        try {
-//            fin = new Scanner(new File(fileName));
-//            while(fin.hasNextLine()){
-//                y++;
-//                //System.out.println(removeWhiteSpace(fin.nextLine()));
-//                String line = fin.nextLine();
-//                System.out.println(removeWhiteSpace(line));
-//                ar[y] = line;
-//                //maze[x][y].add(Splitter(line));
-//
-//
-//
-//            }
-//        } catch (IOException e) {
-//            System.err.println(e);
-//        }
-//
-////        while (fin.hasNextLine()) {
-////            String line = fin.nextLine();
-////            System.out.println(line);
-////            String output = removeWhiteSpace(line);
-////            System.out.println(output);
-//
-////
-////
-////
-////
-////                //This calls the methods which runs the whole selection process
-////
-////            if (!line.equals("0 0 0")) {
-////
-////                System.exit(0);
-////            } else {
-////                System.exit(0);
-////            }
-////        }
-//    }
-//
-//    private static String[] Splitter(String input) {
-//        return input.split(" ");
-//    }
-//
-//
-
--- company/Maze.java
+++ company/Maze.java
@@ -5,18 +5,29 @@ import java.util.List;
 
 public class Maze {
 
-    static int[][] maze = new int[11][11];
-    public static int x = 0;
+    static char[][] maze = new char[12][12];
+
+    static char c;
     public Maze(String line){
 
     }
 
-    public static void generateMaze(char ch, int y){
-        System.out.println();
-        System.out.println("MAZE:"+ ch);
-        System.out.println("MAZE NUMBER: "+x+ ", " +y);
-        maze[x][y] = ch;
-        x++;
+    public static void generateMaze(ArrayList<String> temps){
+        int y = -1;
+        for (String s : temps) {
+            int x = 0;
+            y++;
+            for(int i = 0; i <= (s.length()-1); i++) {
+                System.out.print("Character: "+s.charAt(i));
+                System.out.println();
+                System.out.println("MAZE NUMBER: "+x+ ", " +y);
+                maze[x][y] = s.charAt(i);
+                System.out.println("----------------------------");
+                x++;
+            }
+        }
+
+
 
     }
         public static String removeWhiteSpace(String input) {
@@ -31,15 +42,49 @@ public class Maze {
             return newSentence.toString();
         }
 
-        public static void Print(String input){
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < input.length(); i++) {
-                if (i > 0) {
-                    result.append(" ");
+        public static void Print(){
+            for(int i = 0; i <= 11; i++){
+                System.out.print("\n");
+                for(int j=0; j <= 11; j++){
+                    System.out.print(Maze.maze[j][i] +" ");
                 }
-
-                result.append(input.charAt(i));
             }
-            System.out.println(result.toString());
+        }
+
+
+        public static void mazeRunner(char[][] maze, int x, int y, int handLocX, int handLocY ){
+            int xtemp;
+            int ytemp;
+            if(x == 'F'){
+                System.out.println("Maze Complete");
+            }else{
+
+//                xtemp = x;
+//                ytemp = y;
+//                maze[xtemp][ytemp] = 'x';
+                maze[x][y] = 'x';
+                Print();
+                System.out.println("\n--------------------------");
+                //Making the Move//
+
+                if(maze[x+1][y] == '.'){
+                    x++;
+                    mazeRunner(maze,x,y,handLocX,handLocY);
+                }if(maze[x][y-1]== '.'){
+                    y--;
+                    mazeRunner(maze,x,y,handLocX,handLocY);
+                }if(maze[x][y+1]=='.'){
+                    y++;
+                    mazeRunner(maze,x,y,handLocX,handLocY);
+                }if(maze[x-1][y]=='.'){
+                    x--;
+                    mazeRunner(maze,x,y,handLocX,handLocY);
+
+                }else{}
+
+            }
+
+
+
         }
 }
